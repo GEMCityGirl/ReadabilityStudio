73e68b222ebaafc763926d58e1748ee088d992a6
Blake Madden <madindayton@outlook.com>
2023-04-26 12:39:51 -0400

    Read test files as UTF-8

--- tests/testingmain.cpp
+++ tests/testingmain.cpp
@@ -3,13 +3,17 @@
 #include <catch2/catch_session.hpp>
 #include <fstream>
 #include <iostream>
+#include <filesystem>
+#include <numeric>
 #include "../src/indexing/article.h"
 #include "../src/indexing/stop_lists.h"
 #include "../src/indexing/word_collection.h"
 #include "../src/indexing/spanish_syllabize.h"
 #include "../src/indexing/german_syllabize.h"
+#include "../src/Wisteria-Dataviz/src/utfcpp/source/utf8.h"
 
 using namespace grammar;
+namespace fs = std::filesystem;
 
 word_list Stop_list;
 phrase_collection pcmap;
@@ -30,6 +34,63 @@ word_list Known_spellings;
 word_list Secondary_known_spellings;
 word_list Programming_known_spellings;
 
+//-------------------------------------------------
+bool valid_utf8_file(const std::string& file_name)
+    {
+    std::ifstream ifs(file_name);
+    if (!ifs)
+        { return false; }
+
+    std::istreambuf_iterator<char> it(ifs.rdbuf());
+    std::istreambuf_iterator<char> eos;
+
+    if (utf8::starts_with_bom(it, eos))
+        {
+        std::advance(it, std::size(utf8::bom));
+        }
+
+    return utf8::is_valid(it, eos);
+    }
+
+//-------------------------------------------------
+std::pair<bool, std::wstring> read_utf8_file(const std::string& file_name)
+    {
+    if (!valid_utf8_file(file_name))
+        { return std::make_pair(false, std::wstring{}); }
+
+    std::ifstream fs8(file_name);
+    if (!fs8.is_open())
+        {
+        std::cout << "Could not open " << file_name << "\n";
+        return std::make_pair(false, std::wstring{});
+        }
+
+    unsigned line_count = 1;
+    std::string line;
+    std::wstring buffer;
+    buffer.reserve(static_cast<size_t>(std::filesystem::file_size(file_name)));
+
+    while (std::getline(fs8, line))
+        {
+        if (auto end_it = utf8::find_invalid(line.begin(), line.end());
+            end_it != line.cend())
+            {
+            std::cout << "Invalid UTF-8 encoding detected at line " << line_count << "\n";
+            std::cout << "This part is fine: " << std::string(line.begin(), end_it) << "\n";
+            }
+
+        // Convert it to utf-16
+        std::u16string utf16line = utf8::utf8to16(line);
+        for (const auto& ch : utf16line)
+            { buffer += static_cast<wchar_t>(ch); }
+        buffer += L"\n";
+
+        ++line_count;
+        }
+
+    return std::make_pair(true, buffer);
+    }
+
 // Main entry point for test harness
 //--------------------------------------------
 int main( int argc, char* argv[] )
@@ -49,11 +110,11 @@ int main( int argc, char* argv[] )
 
     // initialize everything
     is_abbreviation::get_abbreviations().load_words(
-        loadResourceFile("WesternAbbr.txt").c_str(), true, false);
+        read_utf8_file("WesternAbbr.txt").second.c_str(), true, false);
     grammar::is_non_proper_word::get_word_list().load_words(
-        loadResourceFile("StopListForProper.txt").c_str(), true, false);
+        read_utf8_file("StopListForProper.txt").second.c_str(), true, false);
     Stop_list.load_words(
-        loadResourceFile("StopListWestern.txt").c_str(), true, false);
+        read_utf8_file("StopListWestern.txt").second.c_str(), true, false);
     grammar::is_english_passive_voice::get_past_participle_exeptions().load_words(
         L"excited\nseven\nheaven\nhaven\nalien\ninfrared\nsacred", true, true);
 
