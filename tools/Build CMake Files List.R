09a4526050dd65b743ba579b2eb65e4b7324f856
Blake-Madden <madindayton@outlook.com>
2023-08-03 18:16:29 -0400

    Build resource from CMake

--- tools/Build CMake Files List.R
+++ tools/Build CMake Files List.R
@@ -3,9 +3,15 @@ library(magrittr)
 library(stringr)
 
 srcFolder <- str_glue('{dirname(rstudioapi::getSourceEditorContext()$path)}/../src')
-buildFile <- str_glue('{dirname(rstudioapi::getSourceEditorContext()$path)}/files.cmake')
+sourcesFile <- str_glue('{dirname(rstudioapi::getSourceEditorContext()$path)}/files.cmake')
 
-# Files for full application
+imagesFolder <- str_glue('{dirname(rstudioapi::getSourceEditorContext()$path)}/../Resources/Images')
+imagesFile <- str_glue('{dirname(rstudioapi::getSourceEditorContext()$path)}/images.cmake')
+
+wordsFolder <- str_glue('{dirname(rstudioapi::getSourceEditorContext()$path)}/../Resources/Words')
+wordsFile <- str_glue('{dirname(rstudioapi::getSourceEditorContext()$path)}/words.cmake')
+
+# Source files for application
 ################################################
 files <- str_glue(
   "src/{list.files(path=srcFolder, pattern='(*[.]cpp|cJSON[.]c|onelua_no_warnings[.]c)', recursive=TRUE)}")
@@ -15,4 +21,17 @@ write_file(str_glue("# Automatically generated from 'Build CMake Files List.R'
 # DO NOT MODIFY MANUALLY!
 
 SET(APP_SRC_FILES\n    {paste(files, collapse='\n    ')})"),
-           file=buildFile)
+           file=sourcesFile)
+
+# Image (and menu) files for resources
+################################################
+files <- str_glue(
+  "{list.files(path=imagesFolder, pattern='*[.](png|jpg|svg|xrc)', recursive=TRUE)}")
+write_file(paste(files, collapse='\n'), file=imagesFile)
+
+# Word lists
+################################################
+files <- str_glue(
+  "{list.files(path=wordsFolder, pattern='*[.]txt', recursive=TRUE)}")
+files <- files[!grepl("(BaseEnglishDictionary.txt|NonPersonalBase.txt|PersonalBase.txt)", files)]
+write_file(paste(files, collapse='\n'), file=wordsFile)
---
+++ tools/words.cmake
@@ -0,0 +1,36 @@
+Abbreviations/List.txt
+Articles/AExceptions.txt
+Articles/AnExceptions.txt
+CitationHeaders/List.txt
+CopyrightNotices/List.txt
+Dictionaries/English.txt
+Dictionaries/German.txt
+Dictionaries/Spanish.txt
+PastParticiples/Exceptions.txt
+Programming/All Languages.txt
+Programming/Assembly.txt
+Programming/CPP.txt
+Programming/CSharp.txt
+Programming/HTML.txt
+Programming/Java.txt
+Programming/Python.txt
+Programming/R.txt
+Programming/VisualBasic.txt
+ProperNouns/All.txt
+ProperNouns/Personal.txt
+StopWords/English.txt
+StopWords/German.txt
+StopWords/ProperNounsStopList.txt
+StopWords/Spanish.txt
+WordLists/Dolch.txt
+WordLists/Harris-Jacobson.txt
+WordLists/New Dale-Chall.txt
+WordLists/Revised Spache.txt
+WordLists/Stocker Catholic Supplement.txt
+WordyPhrases/Dale-ChallReplacements.txt
+WordyPhrases/English.txt
+WordyPhrases/German.txt
+WordyPhrases/Harris-JacobsonReplacements.txt
+WordyPhrases/SingleWordReplacementsEnglish.txt
+WordyPhrases/SpacheReplacements.txt
+WordyPhrases/Spanish.txt
