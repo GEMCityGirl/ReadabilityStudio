6665fe42fc08d2e2d004564f67a181bd0cf39928
Blake-Madden <madindayton@outlook.com>
2023-08-05 07:31:17 -0400

    Add project resources to build, more cmake improvements

---
+++ resource.h
@@ -0,0 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ReadStudio.rc
+//
+#define MANIFEST_RESOURCE_ID            1
+#define wxBITMAP_STD_COLOURS            103
+#define IDI_ICON1                       105
+#define IDI_ICON2                       106
+#define IDI_ICON3                       108
+#define IDI_HELP_ICON                   108
+#define IDI_APP_ICON                    109
+#define ID_Menu                         40001
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        110
+#define _APS_NEXT_COMMAND_VALUE         40002
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
