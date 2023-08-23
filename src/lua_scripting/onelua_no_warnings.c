a9b957907c49e42d3b401854c907c963e2bf3ccc
Blake-Madden <madindayton@outlook.com>
2023-08-23 11:51:04 -0400

    Fix Lua interpretter from being built Fix clang pop section

--- src/lua_scripting/onelua_no_warnings.c
+++ src/lua_scripting/onelua_no_warnings.c
@@ -13,16 +13,22 @@
 #if defined(__clang__)
     // #pragma clang diagnostic ignored ""
 #elif defined(__gcc__)
+    // You need to not use -Wpedantic with GCC;
+    // currently, you can't turn that off this way.
+    // This is because 'ignored' can only ignore specific warnings,
+    // not entire classes of warning like 'all' or 'pendantic'.
     // #pragma GCC diagnostic ignored ""
 #elif defined(_MSC_VER)
     #pragma warning(disable: 4701 4702 4310 4244)
 #endif
 
+// don't build the interpretter, just the library
+#define MAKE_LIB
 #include "../lua/onelua.c"
 
 // POP
-#if defined(__gcc__)
-    #pragma GCC diagnostic pop
+#if defined(__clang__)
+    #pragma clang diagnostic pop
 #elif defined(__gcc__)
     #pragma GCC diagnostic pop
 #elif defined(_MSC_VER)
