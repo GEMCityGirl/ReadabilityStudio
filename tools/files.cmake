3301797cdfcccea772d74e0bb454cb42c4c84aa6
Blake-Madden <madindayton@outlook.com>
2023-09-01 16:40:56 -0400

    Add GTK helper to project

--- tools/files.cmake
+++ tools/files.cmake
@@ -119,6 +119,7 @@ SET(APP_SRC_FILES
     src/Wisteria-Dataviz/src/ui/app.cpp
     src/Wisteria-Dataviz/src/ui/controls/codeeditor.cpp
     src/Wisteria-Dataviz/src/ui/controls/formattedtextctrl.cpp
+    src/Wisteria-Dataviz/src/ui/controls/gtk/gtktextview-helper.cpp
     src/Wisteria-Dataviz/src/ui/controls/htmltablewin.cpp
     src/Wisteria-Dataviz/src/ui/controls/infobarex.cpp
     src/Wisteria-Dataviz/src/ui/controls/listctrlex.cpp
