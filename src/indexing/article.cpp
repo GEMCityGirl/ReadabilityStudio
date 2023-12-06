0f5640e7418d2497976c2489e4f42d397905a978
Blake-Madden <madindayton@outlook.com>
2023-12-05 19:17:07 -0500

    Add article exceptions

--- src/indexing/article.cpp
+++ src/indexing/article.cpp
@@ -295,13 +295,12 @@ bool is_incorrect_english_article::is_a_exception(const wchar_t* word, const siz
         }
 
     static const std::set<traits::case_insensitive_wstring_ex> case_i_u_3_prefixes = {
-        L"ubi", L"uin" /*a unint_32 is correct*/,
-        L"ukr", L"ure",
-        L"uri", L"uro",
-        L"usa", L"use",
-        L"usi", L"usu",
-        L"utc", L"utf",
-        L"uti", L"uto"
+        L"ubi", L"uie", L"uin" /* a unint_32 is correct */,
+        L"ukr", L"ure", L"uri",
+        L"uro", L"usa", L"use",
+        L"usi", L"usu", L"utc",
+        L"utf", L"uti", L"uto",
+        L"uui" /* UUID */
     };
 
     // if a known (full-word, case-insensitive) exception
