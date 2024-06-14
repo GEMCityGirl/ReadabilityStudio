7bc3ec41a9ea5e7b309c823674b4796ffb4da85a
Blake-Madden <66873089+Blake-Madden@users.noreply.github.com>
2024-06-14 05:39:25 -0400

    Ignore "bang bang" Add more comments

--- src/indexing/double_words.cpp
+++ src/indexing/double_words.cpp
@@ -10,6 +10,7 @@
 
 const std::set<grammar::is_double_word_exception::string_type>
     grammar::is_double_word_exception::m_double_word_exceptions = {
+        L"bang",                     // A gun goes bang bang
         L"bison",                    // Bison bison is American bison
         L"Boola",                    // "Boola Boola" is the Yale football song
         L"choo",                     // The train goes "choo choo"
@@ -17,31 +18,31 @@ const std::set<grammar::is_double_word_exception::string_type>
         L"das",      L"der", L"die", // German articles, which can appear repeated
         L"Doki",                     // A videogame
         L"ha",                       // "ha ha ha," she laughed at Blake's hilarious joke
-        L"had",
-        L"ho",       // "Ho ho ho," said Santa
-        L"hush",     // Let's keep that hush hush
-        L"Kappa",    // Kappa Kappa Gamma is a sorority
-        L"Sapiens",  // Homo Sapiens Sapiens is modern humans (subspecies of Homo Sapiens)
-        L"Hool",     // from a Chief Joseph speech
-        L"ice",      // "Ice Ice Baby" is (arguably) as song
-        L"la",       // La La Land
-        L"Leng",     // common name to appear doubled
-        L"log",      // log-log function in math
-        L"long",     // long long integers
-        L"mephitis", // Mephitis mephitis is the white skunk
-        L"no",       // "Using C macros in new code is a no no!"
-        L"now",      // "now now, take it easy"
-        L"Pago",     // capital of American Samoa
-        L"pee",      // "Do you need to pee pee?" asked the urologist
-        L"plus",     // C plus plus (it sometimes gets spelled out like this)
-        L"sie",      // German word that can be repeated
-        L"sigma",    // Sigma Sigma is a fraternity at U. of Cincinnati
-        L"Sirhan",   // "Sirhan Sirhan" was RFJ's assassin
-        L"snip",     // "snip snip," said the urologist
-        L"ta",       // "ta ta" means "goodbye"
-        L"that",
-        L"uh",   // variation of "no"
-        L"Walla" // Walla Walla, Washington
+        L"had",                      // he had had a car for sale
+        L"ho",                       // "Ho ho ho," said Santa
+        L"hush",                     // Let's keep that hush hush
+        L"Kappa",                    // Kappa Kappa Gamma is a sorority
+        L"Sapiens",                  // Homo Sapiens Sapiens is modern humans
+        L"Hool",                     // from a Chief Joseph speech
+        L"ice",                      // "Ice Ice Baby" is (arguably) as song
+        L"la",                       // La La Land
+        L"Leng",                     // common name to appear doubled
+        L"log",                      // log-log function in math
+        L"long",                     // long long integers
+        L"mephitis",                 // Mephitis mephitis is the white skunk
+        L"no",                       // "Using C macros in new code is a no no!"
+        L"now",                      // "now now, take it easy"
+        L"Pago",                     // capital of American Samoa
+        L"pee",                      // "Do you need to pee pee?" asked the urologist
+        L"plus",                     // C plus plus (it sometimes gets spelled out like this)
+        L"sie",                      // German word that can be repeated
+        L"sigma",                    // Sigma Sigma is a fraternity at U. of Cincinnati
+        L"Sirhan",                   // "Sirhan Sirhan" was RFJ's assassin
+        L"snip",                     // "snip snip," said the urologist
+        L"ta",                       // "ta ta" means "goodbye"
+        L"that",                     // I know that that car if for sale
+        L"uh",                       // variation of "no"
+        L"Walla"                     // Walla Walla, Washington
     };
 
 //--------------------------------------------------
