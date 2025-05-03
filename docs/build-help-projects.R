489328917bb9c6edb7456e8ed54e48bd374babc0
Blake-Madden <66873089+Blake-Madden@users.noreply.github.com>
2025-05-03 15:18:37 -0400

    Revert code reorg in help build script R folder needs to be copied after folders are cleared

--- docs/build-help-projects.R
+++ docs/build-help-projects.R
@@ -13,11 +13,6 @@ docFolder <- this.path::this.dir()
 source(glue("{docFolder}/build-enum-files.R"))
 source(glue("{docFolder}/readability-studio-manual/R/appdown.r"))
 
-# copy R script to parent so that single-chapter files can reference it
-dir_copy(glue("{docFolder}/readability-studio-manual/R"),
-         glue("{docFolder}/R"),
-         TRUE)
-
 # delete previous builds
 unlink(glue("{docFolder}/coding-bible/docs"), recursive=T)
 unlink(glue("{docFolder}/shortcuts-cheatsheet/docs"), recursive=T)
@@ -80,6 +75,11 @@ clearFolders <- function()
   }
 
 clearFolders()
+
+# copy R script to parent so that single-chapter files can reference it
+dir_copy(glue("{docFolder}/readability-studio-manual/R"),
+         glue("{docFolder}/R"),
+         TRUE)
 
 # System Admin Manual
 #####################
