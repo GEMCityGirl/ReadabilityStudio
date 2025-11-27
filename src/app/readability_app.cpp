32af63688506a70c355d46e7e67d1d39871effd5
Blake-Madden <66873089+Blake-Madden@users.noreply.github.com>
2025-11-27 09:37:59 -0500

    Update shape name and underlying constant API in Wisteria changed

--- src/app/readability_app.cpp
+++ src/app/readability_app.cpp
@@ -448,7 +448,7 @@ bool ReadabilityApp::OnInit()
                    std::make_pair(_(L"House"), DONTTRANSLATE(L"house")),
                    std::make_pair(_(L"Barn"), DONTTRANSLATE(L"barn")),
                    std::make_pair(_(L"Farm"), DONTTRANSLATE(L"farm")),
-                   std::make_pair(_(L"Dollar"), DONTTRANSLATE(L"dollar")),
+                   std::make_pair(_(L"Hundred dollar bill"), DONTTRANSLATE(L"hundred-dollar-bill")),
                    std::make_pair(_(L"Pumpkin"), DONTTRANSLATE(L"pumpkin")),
                    std::make_pair(_(L"Jack-o'-lantern"), DONTTRANSLATE(L"jack-o-lantern")),
                    std::make_pair(_(L"Monitor"), DONTTRANSLATE(L"monitor")) };
